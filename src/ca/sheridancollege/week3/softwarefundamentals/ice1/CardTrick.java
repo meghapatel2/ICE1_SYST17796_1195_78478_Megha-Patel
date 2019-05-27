872c724cc4203fb4286c1dcd69825c1c61caeb81
masadsaeed <m.asad.saeed4@hotmail.com>
2019-05-27 14:20:10 -0400

    Updating print code and sending to branch

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -19,13 +19,18 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+       for (int i = 0; i < magicHand.length; i++) {
+           
+           Random rnd = new Random();
+           int n1 = rnd.nextInt(13);
+           int n2 = rnd.nextInt(4);
+           Card c = new Card();
+           c.setValue(n1 + 1);
+           c.setSuit(Card.SUITS[n2]);
+
+           System.out.println("Card suit is " + c.getSuit() + " and its value is " + c.getValue() + ".");
            //Then report the result here
-    }
-    
-}
+      }
+
+   }
 }
