a91a1b9c0f0770f216aa4e400664e1766a317c8c
parkhaee <parkhaee@sheridancollege.ca>
2019-05-27 13:44:17 -0400

    Edited the code to display random 7 cards with its values and suits

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,21 +11,30 @@ import java.util.Random;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author Megha
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-           //Then report the result here
-    }
-    
-}
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+      Random rnd = new Random();
+
+
+
+      for (int i = 0; i < magicHand.length; i++) {
+
+         int n1 = rnd.nextInt(13);
+         int n2 = rnd.nextInt(4);
+         Card c = new Card();
+         c.setValue(n1 + 1);
+         c.setSuit(Card.SUITS[n2]);
+
+         System.out.print(c.getSuit() + " " + c.getValue() + "\n");
+         //Then report the result here
+      }
+
+   }
 }
