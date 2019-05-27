f35d6b9f94a450abd7b5bae5c1743ecc175a619a
andrew <flatta@sheridancollege.ca>
2019-05-27 14:19:00 -0400

    Updated the code and now sending to GITHUB Repo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.awt.BorderLayout;
 import java.util.Random;
 
 /**
@@ -19,12 +15,23 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        Card[] suit = new Card[4];
+        
+        Card[] value = new Card[13];
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-           //Then report the result here
+            
+            int random = (int) (Math.random() * 13 + 1);
+            c.setValue(random);
+            
+            int random2 = (int) (Math.random() * 4);
+            
+           c.setSuit(Card.SUITS[random2]);
+           // System.out.println("Your 7 cards are");
+            System.out.println(c.getValue() + "\t" + c.getSuit());
+            //System.out.println(c.getSuit());
     }
     
 }
