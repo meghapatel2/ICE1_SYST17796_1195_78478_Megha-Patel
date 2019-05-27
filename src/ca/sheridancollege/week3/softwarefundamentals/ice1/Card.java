35e69ef1eb3edd4f1e7ba769edb640126e50b915
andrew <flatta@sheridancollege.ca>
2019-05-27 14:20:05 -0400

    Updated the code and now sending to GITHUB Repo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
 
 /**
  * A class that models playing card Objects. Cards have 
@@ -12,17 +8,21 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author Megha
+ * @author AndrewFlatt
+ * Student# 991279407
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value = Math.random(13); //1-13
+   private String suit = ("Clubs, Spades, Diamonds, Hearts"); //clubs, spades, diamonds, hearts
+   private int value; //1-13
+   //int random = (int) (Math.random() * 13 + 1);
+   
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
+   
       
     public String getSuit() {
         return suit;
