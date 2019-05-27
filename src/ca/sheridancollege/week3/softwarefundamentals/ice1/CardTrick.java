5443a23814f30f87e9b331723a9074cf0b7f3363
sharante <sharante@shernet.sheridancollege.ca>
2019-05-27 14:21:01 -0400

    Made changes in code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,21 +11,41 @@ import java.util.Random;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author Megha
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-           //Then report the result here
-    }
-    
-}
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         Random rnd = new Random();
+         int value = rnd.nextInt(13) + 1;
+         c.setValue(value);
+         int a = rnd.nextInt(3);
+         c.setSuit(Card.SUITS[a]);
+         if (value == 1) {
+            System.out.println("Ace of " + c.getSuit());
+         }
+         else if (value == 11) {
+            System.out.println("Joker of " + c.getSuit());
+         }
+         else if (value == 12) {
+            System.out.println("Quuen of " + c.getSuit());
+         }
+         else if (value == 13) {
+            System.out.println("King of " + c.getSuit());
+         }
+         else {
+            System.out.println(c.getValue() + " of " + c.getSuit());
+
+         }
+         //Then report the result here
+      }
+
+   }
 }
