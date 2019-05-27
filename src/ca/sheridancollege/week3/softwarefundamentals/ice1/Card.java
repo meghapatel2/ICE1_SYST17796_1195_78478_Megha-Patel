51f7677262e2187cc11ef9b1e5d1ef07f6f2068c
masadsaeed <m.asad.saeed4@hotmail.com>
2019-05-27 14:20:26 -0400

    Updating print code and sending to branch

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -17,12 +17,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
+   
+   private int value;
+   
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
+   /**
      * @return the suit
      */
+   
     public String getSuit() {
         return suit;
     }
@@ -45,9 +47,13 @@ public class Card {
      * @param value the value to set
      */
     public void setValue(int value) {
+        
         this.value = value;
     }
-   
+    
+    public void printResults() {
+        
+    }
    
     
 }
