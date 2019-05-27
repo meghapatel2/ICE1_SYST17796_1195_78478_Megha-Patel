bb3407807e3a513710efa9dd4900dd1ec40a249c
Jennie Jisung Kang <Jennie Jisung Kang@DESKTOP-3BO97K5>
2019-05-27 13:46:56 -0400

    first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -22,10 +22,15 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            c.setValue ( (int)Math.random() *13 +1);
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
            //Then report the result here
-    }
+           
+ 
+      
     
 }
+    
 }
+
